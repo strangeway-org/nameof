2a92c5c827cce5ac0dc3982243c4a9579fbc4cd3
Yuriy Artamonov <artamonov@haulmont.com>
2017-10-11 08:00:44 +0400

    Apache License 2.0

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 Java nameOfProperty implementation
 ==================================
 
-Java alternative to nameOf operator. Based on Byte Buddy library.
+Java alternative to nameOf operator. 
+
+[![license](https://img.shields.io/badge/license-Apache%20License%202.0-blue.svg?style=flat)](http://www.apache.org/licenses/LICENSE-2.0)
+[![Download](https://api.bintray.com/packages/strangeway-org/libs/nameof/images/download.svg) ](https://bintray.com/strangeway-org/libs/nameof/_latestVersion)
+
+Based on Byte Buddy library. 
 
 Inspired by: http://in.relation.to/2016/04/14/emulating-property-literals-with-java-8-method-references/
 
--- src/main/java/org/strangeway/nameof/LangUtils.java
+++ src/main/java/org/strangeway/nameof/LangUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017 Yuriy Artamonov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package org.strangeway.nameof;
 
 import org.strangeway.nameof.impl.PropertyNameExtractor;
@@ -5,6 +21,9 @@ import org.strangeway.nameof.impl.PropertyNames;
 
 import java.util.function.Function;
 
+/**
+ * Utility class that can provide name of bean property using method reference of a getter.
+ */
 public final class LangUtils {
     public static <T> String nameOfProperty(Class<T> clazz, Function<? super T, ?> bridge) {
         T extractor = PropertyNames.getPropertyNameExtractor(clazz);
